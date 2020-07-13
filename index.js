4236d74c9f3a341c23d2631239f494de198ab561
MossW <59309429+MossW268@users.noreply.github.com>
2020-07-13 19:57:44 +0100

    Front Page Backend!

---
+++ index.js
@@ -0,0 +1,8 @@
+function seperate() {
+  var x = document.getElementById("imageToSeperate");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
