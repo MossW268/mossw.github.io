45f10022e8dfa3340db40f9e9608498de50e37a9
MossW <59309429+MossW268@users.noreply.github.com>
2020-07-13 20:06:55 +0100

    Delete seperate.js

--- seperate.js
+++
@@ -1,8 +0,0 @@
-function seperate() {
-  var x = document.getElementById("imageToSeperate");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
